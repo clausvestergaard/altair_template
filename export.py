33e310ff8d9613e142ed3d21761dad3dd1dfa7f7
Claus Vestergaard <clausvestergaard@outlook.com>
2018-10-28 12:35:32 +0100

    WIP: Added a few methods for validating I/O.

--- export.py
+++ export.py
@@ -5,9 +5,11 @@ import pathlib
 
 class ExportingData():
     exporting = []
-    data_Keys = "'type, 'data'"
+    data_Keys = ['type', 'data']
+    directory_path = f'{pathlib.Path(os.getcwd()).joinpath("obj")}'
+    file_path = f'{directory_path.joinpath("data.json")}'
 
-    def add_data(self, type: str, data: str):
+    def add_data(self, data_type: str, data_values: str):
         """
         Function for adding data to the object prior to exporting the data and running the app.
 
@@ -26,28 +28,46 @@ class ExportingData():
         Examples
         --------
         >>> ED = ExportingData()
-        >>> ED.add_data(type='figur', data=chart_cars.to_json())
-        >>> ED.add_data(type='tabel', data=cars.head().to_html(classes='table is-striped is-hoverable')
+        >>> ED.add_data(data_type='figur', data_values=chart_cars.to_json())
+        >>> ED.add_data(data_type='tabel', data_values=cars.head().to_html(classes='table is-striped is-hoverable')
         >>> print(ED.exporting)
         [xxxx]
         """
 
-        #if self.validate_input(type, data):
-        _data = {'type': type, 'data': data}
-        self.exporting.append(_data)
+        if self.validate_input(data_type, data_values):
+            _data = {'type': data_type, 'data': data_values}
+            self.exporting.append(_data)
         return self
 
     def save_data(self):
-        path = f'{pathlib.Path(os.getcwd()).joinpath("obj/data.json")}'
-        with open(path, 'w') as f:
+        with open(self.file_path, 'w') as f:
             json.dump(self.exporting, f)
 
-    #def validate_input(self, data) -> bool:
-    #    try:
-    #        assert isinstance(data, dict)
-    #    except AssertionError:
-    #        print("You have to add the data in the format of a dict with the following keys")
-    #        return False
+    def validate_input(self, data_type, data_values) -> bool:
+        try:
+            assert isinstance(data_type, str)
+            assert isinstance(data_values, str)
+        except AssertionError:
+            print(
+                f'Inputs to the method {ExportingData.__name__}.{ExportingData.add_data.__name__} have to be strings.')
+        return True
 
-# isinstance(data, list)
-# {k for d in LoD for k in d.keys()}
+    def validate_output(self):
+        try:
+            assert os.path.isdir(self.directory_path)
+        except AssertionError:
+            print(f'The directory {self.directory_path} does not exist.')
+            return False
+
+        try:
+            assert isinstance(self.exporting, list)
+        except AssertionError:
+            print("Error message.")
+            return False
+
+        try:
+            assert all([isinstance(i, dict) for i in self.exporting])
+        except AssertionError:
+            print("Error message")
+            return False
+        return True
