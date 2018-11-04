0db23e25f11ca26e0a6f91491ad0a222f4ff5875
Claus Vestergaard <clausvestergaard@outlook.com>
2018-11-04 09:58:12 +0100

    Made a few changes to validation of inputs and output

--- export.py
+++ export.py
@@ -18,6 +18,7 @@ class ExportingData():
     def add_data(self, data_type: str, data_values: str):
         """
         Function for adding data to the object prior to exporting the data and running the app.
+        Basically just validating input and appending dictionaries to the list 'exporting'.
 
         Parameters
         ----------
@@ -27,25 +28,22 @@ class ExportingData():
             Data represented as a string. Json if the data represents an Altair chart or in HTML if the data
             represents a table.
 
-        Returns
-        -------
-        dict
 
         Examples
         --------
         >>> ED = ExportingData()
         >>> ED.add_data(data_type='figur', data_values=chart_cars.to_json())
         >>> ED.add_data(data_type='tabel', data_values=cars.head().to_html(classes='table is-striped is-hoverable')
-        >>> print(ED.exporting)
-        [xxxx]
         """
 
-        if self.validate_input(data_type, data_values):
-            _data = {'type': data_type, 'data': data_values}
-            self.exporting.append(_data)
+        self.validate_input(data_type, data_values)
+
+        _data = {'type': data_type, 'data': data_values}
+        self.exporting.append(_data)
         return self
 
     def save_data(self):
+        self.validate_output()
         with open(self.file_path, 'w') as f:
             json.dump(self.exporting, f)
 
@@ -54,21 +52,12 @@ class ExportingData():
             assert isinstance(data_type, str)
             assert isinstance(data_values, str)
         except AssertionError:
-            print(
-                f'Inputs to the method {ExportingData.__name__}.{ExportingData.add_data.__name__} have to be strings.')
-            return False
-        return True
+            raise TypeError('Both data_type and data_values have to be of type str. '
+                            f'Input was data_type: {type(data_type)} and data_values: {type(data_values)}.')
 
     def validate_output(self):
         try:
             assert isinstance(self.exporting, list)
-        except AssertionError:
-            print("Error message.")
-            return False
-
-        try:
             assert all([isinstance(i, dict) for i in self.exporting])
         except AssertionError:
-            print("Error message")
-            return False
-        return True
+            raise TypeError('exporting has to be a list of dictionaries.')
